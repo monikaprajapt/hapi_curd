8f107c1bd1fc92c37718b0c4e8cc7158ef2c130a
MonikaPrajapati <92983459+monikaprajapt@users.noreply.github.com>
2022-07-11 12:30:42 +0530

    Add files via upload

---
+++ 20220709122852_init.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("hapi_table", (table) => {
+        table.increments('id').primary(),
+        table.string('name'),
+        table.string('email'),
+        table.string('password')
+
+    });
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
